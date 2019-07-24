0b5447195649c88440ba758a22565445722e945a
Astrolopithecus <47542033+Astrolopithecus@users.noreply.github.com>
2019-07-24 14:53:36 -0700

    update 1
    now ignores non-letter characters

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,23 @@
+# Miles Philips
+# Prog 260
+# Recursive program to check if a string is a palindrome
+# 7-22-19 
+
+from string import ascii_letters
+
 def reverseString(word):
-    return word [::-1]
+    """returns the reverse of a string
+    runtime is (O)n"""
+    if len(word) < 2:
+        return word
+    else:
+        return word [::-1]
 
 def isPalindrome(word):
+    """tests whether or not a string is a palindrome
+    runtime is (O)n""" 
     if len(word) < 2:
         return True
     else:
-        return word == reverseString(word)
+        new = ''.join(char.lower() for char in word if char in ascii_letters)
+        return new == reverseString(new)
